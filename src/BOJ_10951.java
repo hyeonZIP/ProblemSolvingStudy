87606e359ef3f514b9448707a7d1f1acb5073417
hyeonZIP <139322256+hyeonZIP@users.noreply.github.com>
2024-07-24 16:57:43 +0900

    first

---
+++ src/BOJ_10951.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class BOJ_10951 {
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        while(true)
+        {
+            String s = br.readLine();
+
+            if(s == null || s.isEmpty())
+            {
+                break;
+            }
+
+            StringTokenizer st = new StringTokenizer(s);
+
+            sb.append(Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken())).append("\n");
+        }
+        System.out.println(sb);
+        br.close();
+    }
+}
