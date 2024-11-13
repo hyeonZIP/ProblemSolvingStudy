642cd781ffcfe3f9204e892a7d5690b27fe711f7
hyeonZIP <aminez4466@gmail.com>
2024-11-13 22:49:16 +0900

    feat: new solving

---
+++ src/SWExpertAcademy/SWEA_21425.java
@@ -0,0 +1,42 @@
+package SWExpertAcademy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class SWEA_21425 {
+    public static void main(String[] args) throws IOException {
+        /**
+         * SWEA_21425 : +=
+         * 항상 작은 값에 더해주기
+         */
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+
+        for (int i=0; i<T; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            int N = Integer.parseInt(st.nextToken());
+
+            int count = 0;
+            while(A<=N && B<=N){
+                if (A>=B){
+                    B+=A;
+                    count++;
+                }
+                else {
+                    A+=B;
+                    count++;
+                }
+            }
+            sb.append(count).append("\n");
+        }
+        System.out.println(sb);
+    }
+
+
+}
