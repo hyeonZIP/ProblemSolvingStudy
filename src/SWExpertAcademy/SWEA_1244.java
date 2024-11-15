9683ffdda4164d9a72d23d95ffec4159cef48b20
hyeonZIP <aminez4466@gmail.com>
2024-11-16 05:55:27 +0900

    feat: DFS, BackTracking

--- src/SWExpertAcademy/SWEA_1244.java
+++ src/SWExpertAcademy/SWEA_1244.java
@@ -2,6 +2,11 @@ package SWExpertAcademy;
 
 import java.util.Scanner;
 
+/**
+ * SWEA_1244 : [S/W 문제해결 응용] 2일차 - 최대 상금 [D3]
+ * DFS 와 BackTracking(원상복구) 활용
+ */
+
 public class SWEA_1244 {
     static int arr[];
     static int N;
