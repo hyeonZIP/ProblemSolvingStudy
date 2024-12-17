a2d9c11f14d9b17eab1ffdf1416ae5f426679d05
hyeonZIP <aminez4466@gmail.com>
2024-12-17 16:55:40 +0900

    feat: Hash

---
+++ src/Hash/Programmers_1845_Lv1.java
@@ -0,0 +1,46 @@
+package Hash;
+
+import java.io.*;
+import java.util.HashMap;
+
+public class Programmers_1845_Lv1 {
+
+    static int[] input;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        while (true) {
+            try {
+                stringToArray(br.readLine());
+                System.out.println(solution(input));
+            } catch (IOException e) {
+                break;
+            }
+        }
+    }
+
+    /**
+     * 입력값 정수형 배열로 변환
+     */
+    public static void stringToArray(String string) {
+        String[] stringArray = string.substring(1, string.length() - 1).split(",");
+        input = new int[stringArray.length];
+        for (int i = 0; i < stringArray.length; i++) {
+            input[i] = Integer.parseInt(stringArray[i]);
+        }
+    }
+
+    public static int solution(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+        }
+
+        int ponketmonType = map.keySet().size();
+        int takableNumber = nums.length / 2;
+
+        return Math.min(ponketmonType, takableNumber);
+    }
+}
