33973f71d1d3320808209d8e3e35343b8d4513d0
hyeonZIP <aminez4466@gmail.com>
2024-12-17 17:04:34 +0900

    comment: 주석 변경

--- src/Hash/Programmers_1845_Lv1.java
+++ src/Hash/Programmers_1845_Lv1.java
@@ -21,7 +21,8 @@ public class Programmers_1845_Lv1 {
     }
 
     /**
-     * 입력값 정수형 배열로 변환
+     * 문자열을 정수형 배열로 변환
+     * [3,1,2,3] -> int[] nums = {3,1,2,3}
      */
     public static void stringToArray(String string) {
         String[] stringArray = string.substring(1, string.length() - 1).split(",");
